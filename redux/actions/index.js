a1c4cf9374742bbd73fe9669ffc3811ea982fa7d
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-09-29 12:50:33 -0700

    Fetch lists from backend

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,4 +1,10 @@
-import { SET_HAS_SPLASH_SCREEN_BEEN_SHOWN } from '../action-types'
+import axios from 'axios'
+import {
+  FETCH_LISTS_AND_TASKS_REQUEST_SENT,
+  FETCH_LISTS_AND_TASKS_REQUEST_SUCCEEDED,
+  FETCH_LISTS_AND_TASKS_REQUEST_FAILED,
+  SET_HAS_SPLASH_SCREEN_BEEN_SHOWN,
+} from '../action-types'
 
 export const setHasSplashScreenBeenShown = (hasSplashScreenBeenShown) => ({
   type: SET_HAS_SPLASH_SCREEN_BEEN_SHOWN,
@@ -6,3 +12,77 @@ export const setHasSplashScreenBeenShown = (hasSplashScreenBeenShown) => ({
     hasSplashScreenBeenShown,
   },
 })
+
+// TEST ALL OF THIS, DUMMY!
+
+export const fetchListsAndTasksRequestSent = () => ({
+  type: FETCH_LISTS_AND_TASKS_REQUEST_SENT,
+})
+
+export const fetchListsAndTasksRequestFailed = () => ({
+  type: FETCH_LISTS_AND_TASKS_REQUEST_FAILED,
+})
+
+export const fetchListsAndTasksRequestSucceeded = ({ listsById, tasksById }) => ({
+  type: FETCH_LISTS_AND_TASKS_REQUEST_SUCCEEDED,
+  payload: {
+    listsById,
+    tasksById,
+  },
+})
+
+export const fetchListsAndTasks = () => async (dispatch) => {
+  dispatch(fetchListsAndTasksRequestSent())
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: 'https://blueberry-pudding-19740.herokuapp.com/graphql',
+      data: {
+        query: `
+          query {
+            lists {
+              id
+              title
+              tasks {
+                id
+              }
+            }
+            tasks {
+              id
+              description
+              isDone
+            }
+          }
+        `,
+      },
+    })
+    const listsById = response.data.data.lists.reduce(
+      (cumulativeLists, currentList) => {
+        const listWithTaskIds = {
+          ...currentList,
+          taskIds: currentList.tasks.map(task => task.id)
+        }
+        delete listWithTaskIds.tasks
+        return {
+          ...cumulativeLists,
+          [currentList.id]: listWithTaskIds
+        }
+      },
+      {},
+    )
+    const tasksById = response.data.data.tasks.reduce(
+      (cumulativeTasks, currentTask) => {
+        return {
+          ...cumulativeTasks,
+          [currentTask.id]: currentTask,
+        }
+      },
+      {},
+    )
+    dispatch(fetchListsAndTasksRequestSucceeded({ listsById, tasksById }))
+  } catch (error) {
+    console.error(error)
+    dispatch(fetchListsAndTasksRequestFailed())
+    throw error
+  }
+}
---
+++ redux/reducers/entities/lists/isLoading/index.test.js
@@ -0,0 +1,35 @@
+import isLoading from './index'
+import {
+  fetchListsAndTasksRequestSent,
+  fetchListsAndTasksRequestFailed,
+  fetchListsAndTasksRequestSucceeded,
+} from '../../../../actions'
+
+describe('lists.isLoading', () => {
+  describe('FETCH_LISTS_AND_TASKS_REQUEST_SENT', () => {
+    it('indicates that the lists are loading', () => {
+      const initialState = false
+      const action = fetchListsAndTasksRequestSent()
+      const newState = isLoading(initialState, action)
+      expect(newState).toBe(true)
+    })
+  })
+
+  describe('FETCH_LISTS_AND_TASKS_REQUEST_FAILED', () => {
+    it('indicates that the lists are no longer loading', () => {
+      const initialState = true
+      const action = fetchListsAndTasksRequestFailed()
+      const newState = isLoading(initialState, action)
+      expect(newState).toBe(false)
+    })
+  })
+
+  describe('FETCH_LISTS_AND_TASKS_REQUEST_SUCCEEDED', () => {
+    it('indicates that the lists are no longer loading', () => {
+      const initialState = true
+      const action = fetchListsAndTasksRequestSucceeded({ listsById: {}, tasksById: {} })
+      const newState = isLoading(initialState, action)
+      expect(newState).toBe(false)
+    })
+  })
+})
